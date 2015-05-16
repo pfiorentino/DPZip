8b0501057c84bf99e564e5d07cefe96f7afa9511
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2015-05-16 22:20:32 +0200

    Add file listing

--- DPZip.pro
+++ DPZip.pro
@@ -1,9 +1,13 @@
 TEMPLATE = app
 CONFIG += console
 CONFIG -= app_bundle
-CONFIG -= qt
+CONFIG += qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    filepool.cpp \
+
+HEADERS += \
+    filepool.h \
 
 QMAKE_CXXFLAGS += -std=c++0x
 QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.9
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "filepool.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Building pool" << endl;
+
+    FilePool pool("/Users/paul/test_cpp");
+
+    cout << "Done" << endl;
+    cout << "Number of files: " << pool.count() << endl;
+
+
     return 0;
 }
 
